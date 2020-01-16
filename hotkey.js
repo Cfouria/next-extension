ba7fa49b82123acb5ba8d581be5aafe65b643d7f
Cfouria <cat.alucardo@gmail.com>
2020-01-16 03:19:26 -0600

    feature failed

--- hotkey.js
+++ hotkey.js
@@ -1,8 +1,7 @@
 window.addEventListener("keydown", function(e){
 	if(e.ctrlKey && e.keyCode == 69){
 		browser.storage.sync.get('color', function(data) {
-			var thisAnnoysMe = data.color;
-			[...document.getElementsByTagName("a")].map(e=>e.textContent.includes(thisAnnoysMe) ? e.click() : false)
+			[...document.getElementsByTagName("a")].map(e=>e.textContent.includes(data.color) ? e : false).filter(e=>e).map((e,i)=>!i&&e?e.click():false)
 		});
 	};
 });
